7ddfd2086b8cad4c648804f6a13d9a67a5796280
Roman <sher-rom2013@yandex.ru>
2025-11-29 17:06:33 +0700

    Fix bug in content links, removed unwanted script

--- Common/linux_user_config.sh
+++ Common/linux_user_config.sh
@@ -5,26 +5,30 @@ _GREEN_COLOR="\e[1;32m"
 _YELLOW_COLOR="\e[1;33m"
 _COLOR_RESET="\e[0m"
 
-function report_step {
-    echo -e "${_YELLOW_COLOR}[STEP] $1...${_COLOR_RESET}"
+report_step() {
+    echo
+    echo "[STEP] $1..."
 }
 
-function report_success {
-    echo -e "  ${_GREEN_COLOR}[SUCCESS] $1.${_COLOR_RESET}"
+report_success() {
+    echo -e "${_GREEN_COLOR}[SUCCESS] $1${_COLOR_RESET}"
 }
 
-function report_fail {
-    echo -e "  ${_RED_COLOR}[FAIL] $1!${_COLOR_RESET}"
+report_warning() {
+    echo -e "${_YELLOW_COLOR}[SUCCESS] $1${_COLOR_RESET}"
 }
 
+report_fail() {
+    echo -e "${_RED_COLOR}[FAIL] $1${_COLOR_RESET}"
+}
+
+# Аргумент 1: URL скачиваемого файла
+# Аргумент 2: Путь для скачивания файла
+# Так как wget не умеет перезаписывать файлы, нужно сначала удалить файл при его наличии
 function config_download {
-    # Аргумент 1: URL скачиваемого файла
-    # Аргумент 2: Путь для скачивания файла
-    # Так как wget не умеет перезаписывать файлы, нужно сначала удалить файл при его наличии
     if [ -f $2 ]; then
         rm -f $2 &> /dev/null
     fi
-    # Скачивание файла
     file_name=$(echo $1 | awk -F '/' '{print $NF}')
     if wget $1 -O $2 &> /dev/null; then
         report_success "Файл $file_name был успешно скачан по адресу $2"
@@ -35,10 +39,7 @@ function config_download {
 
 report_step "Создание необходимых директорий"
 for directory in \
-    $HOME/.vim/colors \
-    $HOME/.config/mc \
-    $HOME/.local/share/mc/skins \
-    $HOME/.config/btop;
+    $HOME/.config/mc;
 do
     if [[ ! -d $directory ]]; then
         if mkdir -p $directory &> /dev/null; then
@@ -47,19 +48,23 @@ do
             report_fail "При создании директории \"$directory\" произошла ошибка"
         fi
     else
-        report_success "Директория \"$directory\" была успешно создана"
+        report_warning "Директория \"$directory\" уже существует"
     fi
 done
 
 report_step "Установка необходимых конфигов, скриптов, тем"
 config_download https://raw.githubusercontent.com/rsherstnev/LinuxConfigs/master/bash/.bashrc $HOME/.bashrc
-config_download https://raw.githubusercontent.com/rsherstnev/LinuxConfigs/refs/heads/master/bash_and_zsh/.aliases $HOME/.aliases
-config_download https://raw.githubusercontent.com/rsherstnev/LinuxConfigs/refs/heads/master/bash_and_zsh/.functions $HOME/.functions
 config_download https://raw.githubusercontent.com/rsherstnev/LinuxConfigs/master/bash/.inputrc $HOME/.inputrc
+config_download https://raw.githubusercontent.com/rsherstnev/LinuxConfigs/master/common/.aliases $HOME/.aliases
+config_download https://raw.githubusercontent.com/rsherstnev/LinuxConfigs/master/common/.functions $HOME/.functions
 config_download https://raw.githubusercontent.com/rsherstnev/LinuxConfigs/master/vim/.vimrc $HOME/.vimrc
-config_download https://raw.githubusercontent.com/cocopon/iceberg.vim/master/colors/iceberg.vim $HOME/.vim/colors/iceberg.vim
 config_download https://raw.githubusercontent.com/rsherstnev/LinuxConfigs/master/tmux/.tmux.conf $HOME/.tmux.conf
-config_download https://raw.githubusercontent.com/dracula/midnight-commander/master/skins/dracula256.ini $HOME/.local/share/mc/skins/dracula256.ini
-config_download https://raw.githubusercontent.com/rsherstnev/LinuxConfigs/master/mc/ini $HOME/.config/mc/ini
-config_download https://raw.githubusercontent.com/rsherstnev/LinuxConfigs/master/mc/panels.ini $HOME/.config/mc/panels.ini
-config_download https://raw.githubusercontent.com/rsherstnev/LinuxConfigs/master/btop/btop.conf $HOME/.config/btop/btop.conf
+
+if [[ $EUID == 0 ]]; then
+    home_dir=root
+else
+    home_dir=user
+fi
+
+config_download https://raw.githubusercontent.com/rsherstnev/LinuxConfigs/master/mc/$home_dir/ini $HOME/.config/mc/ini
+config_download https://raw.githubusercontent.com/rsherstnev/LinuxConfigs/master/mc/$home_dir/panels.ini $HOME/.config/mc/panels.ini
